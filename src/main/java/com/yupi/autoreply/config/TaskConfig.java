91334274bfe7d1f289d6b9aae822e24bb6cc5dff
yupi <592789970@qq.com>
2023-03-27 10:33:25 +0800

    add 支持并发配置

--- src/main/java/com/yupi/autoreply/config/TaskConfig.java
+++ src/main/java/com/yupi/autoreply/config/TaskConfig.java
@@ -4,6 +4,7 @@ import com.yupi.autoreply.job.JobMediator;
 import com.yupi.autoreply.model.TaskListItem;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.BooleanUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +12,8 @@ import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.Executors;
 
 /**
  * 任务配置
@@ -25,12 +28,22 @@ import java.util.List;
 public class TaskConfig implements SchedulingConfigurer {
 
     /**
+     * 并发配置
+     */
+    private ConcurrentConfig concurrent = new ConcurrentConfig();
+
+    /**
      * 任务列表
      */
     private List<TaskListItem> list;
 
     @Override
     public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+        if (BooleanUtils.isTrue(concurrent.getEnable())) {
+            // 如果开启并发，默认并发度为任务数，即全并发，可通过配置更改
+            int size = Optional.ofNullable(concurrent.getSize()).orElse(list.size());
+            taskRegistrar.setScheduler(Executors.newScheduledThreadPool(size));
+        }
         log.info("--- 任务注册开始 ---");
         for (int i = 0; i < list.size(); i++) {
             TaskListItem taskListItem = list.get(i);
@@ -41,5 +54,12 @@ public class TaskConfig implements SchedulingConfigurer {
             log.info("任务注册成功 {}", taskListItem);
         }
         log.info("--- 任务注册结果 ---");
+    }
+
+    @Data
+    public static class ConcurrentConfig {
+        private Boolean enable = false;
+
+        private Integer size;
     }
 }
